51974ce3060b169154524f62b8735205b625befb
Sunil Sankar <sunil@sunil.cc>
2022-07-08 11:49:00 +0200

    Update install.sh

--- tasks/install.sh
+++ tasks/install.sh
@@ -4,8 +4,8 @@
 #Author Sunil Sankar
 apt update -y
 apt install sshpass podman python3 python3-pip gcc git cmake make gcc vim curl -y
-su - vagrant -c "podman --storage-opt overlay.ignore_chown_errors=true pull docker.io/sunilsankar/docker-almalinux"
-su - vagrant -c "podman --storage-opt overlay.ignore_chown_errors=true pull docker.io/sunilsankar/docker-rocky8"
+su - vagrant -c "podman  pull docker.io/sunilsankar/docker-almalinux"
+su - vagrant -c "podman  pull docker.io/sunilsankar/docker-rocky8"
 pip install --upgrade pip setuptools
 
 pip install -r /vagrant/tasks/files/requirements.txt
